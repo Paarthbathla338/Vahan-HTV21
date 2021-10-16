f284a898a5c27a5281ae7b3ee5cbece7a5b295fc
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-10-16 23:25:52 +0530

    Issue Resolved

--- components/Categories.js
+++ components/Categories.js
@@ -1,24 +1,24 @@
 import React from 'react'
-import { View, Text,TouchableOpacity } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import Ionicons from "react-native-vector-icons/Ionicons"
 
 export default function Categories() {
     return (
-        <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly',padding:15,backgroundColor: '#00b3b3', borderBottomRightRadius: 35,borderTopRightRadius: 35,flex:1,height:"100%",width:90,marginLeft:5}}>
+        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-evenly', padding: 15, backgroundColor: '#00b3b3', borderBottomRightRadius: 35, borderBottomLeftRadius: 35}}>
 
-            <TouchableOpacity style={{ alignItems: "center",justifyContent:"center", marginRight: 30,marginLeft: 30, resizeMode: "contain", marginTop: 5 }} >
+            <TouchableOpacity style={{ alignItems: "center", justifyContent: "center", marginRight: 30, marginLeft: 30, resizeMode: "contain", marginTop: 5 }} >
                 <Ionicons name="fitness-outline" size={24} paddingRight={10} color="red" />
-                <Text style={{ fontSize: '13', fontWeight: '900',textAlign:"center", color:"white"}}>Med Support</Text>
+                <Text style={{ fontSize: '13', fontWeight: '900', textAlign: "center", color: "white" }}>Med Support</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={{ alignItems: "center", marginRight: 30,marginLeft: 30, resizeMode: "contain", marginTop: 5 }} >
+            <TouchableOpacity style={{ alignItems: "center", marginRight: 30, marginLeft: 30, resizeMode: "contain", marginTop: 5 }} >
                 <Ionicons name="bandage-outline" size={24} paddingRight={10} color="grey" />
-                <Text style={{ fontSize: '13', fontWeight: '900' ,textAlign:"center",color:"white"}}>Counselling</Text>
+                <Text style={{ fontSize: '13', fontWeight: '900', textAlign: "center", color: "white" }}>Counselling</Text>
             </TouchableOpacity>
 
-            <TouchableOpacity style={{ alignItems: "center", marginRight: 30,marginLeft: 30, resizeMode: "contain", marginTop: 5 }} >
+            <TouchableOpacity style={{ alignItems: "center", marginRight: 30, marginLeft: 30, resizeMode: "contain", marginTop: 5 }} >
                 <Ionicons name="happy-outline" size={24} paddingRight={10} color="#FFBF00" />
-                <Text style={{ fontSize: '13', fontWeight: '900',textAlign:"center" ,color:"white"}}>Therapy</Text>
+                <Text style={{ fontSize: '13', fontWeight: '900', textAlign: "center", color: "white" }}>Therapy</Text>
             </TouchableOpacity>
 
 
--- screens/Home.js
+++ screens/Home.js
@@ -1,17 +1,23 @@
 import React from 'react'
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import Categories from '../components/Categories'
 import Header from "../components/Header"
 import SearchBar from "../components/SearchBar"
+import RestaurantItem from "../components/RestaurantItem"
 
 export default function Home() {
     return (
-        <SafeAreaView style={{backgroundColor:"#eeee",flex:1}}>
-        <View style={{backgroundColor:"white",padding:15}}>
-        <Header/>
-        <SearchBar/>
+        <SafeAreaView style={{ backgroundColor: "#eeee", flex: 1 }}>
+            <View style={{ backgroundColor: "white", padding: 15 }}>
+                <Header />
+                <SearchBar />
 
 
-        </View>
+            </View>
+            <View>
+                <Categories />
+                <RestaurantItem />
+            </View>
         </SafeAreaView>
     )
 }
