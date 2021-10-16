f151dd6ae786fbd27815c00bc303b405afd1c550
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-10-16 10:02:06 +0530

    Added Important Files

---
+++ screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import Header from "../components/Header"
+
+export default function Home() {
+    return (
+        <SafeAreaView>
+        <Header/>
+        </SafeAreaView>
+    )
+}
