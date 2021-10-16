5ea4c458a431a371829b870443fbf5965a044786
Paarthbathla338 <Paarth.bathla@outlook.com>
2021-10-16 20:37:09 +0530

    Design for Categories Completed

--- components/Categories.js
+++ components/Categories.js
@@ -1,25 +1,25 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text,TouchableOpacity } from 'react-native'
 import Ionicons from "react-native-vector-icons/Ionicons"
 
 export default function Categories() {
     return (
-        <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+        <View style={{ flexDirection: 'column', alignItems: 'center', justifyContent: 'space-evenly',padding:15,backgroundColor: '#00b3b3', borderBottomRightRadius: 35,borderTopRightRadius: 35,flex:1,height:"100%",width:90,marginLeft:5}}>
 
-            <View style={{ alignItems: "center", marginRight: 30, resizeMode: "contain", marginTop: 5 }} >
+            <TouchableOpacity style={{ alignItems: "center",justifyContent:"center", marginRight: 30,marginLeft: 30, resizeMode: "contain", marginTop: 5 }} >
                 <Ionicons name="fitness-outline" size={24} paddingRight={10} color="red" />
-                <Text style={{ fontSize: '13', fontWeight: '900' }}>Med-Support</Text>
-            </View>
+                <Text style={{ fontSize: '13', fontWeight: '900',textAlign:"center", color:"white"}}>Med Support</Text>
+            </TouchableOpacity>
 
-            <View style={{ alignItems: "center", marginRight: 30, resizeMode: "contain", marginTop: 5 }} >
+            <TouchableOpacity style={{ alignItems: "center", marginRight: 30,marginLeft: 30, resizeMode: "contain", marginTop: 5 }} >
                 <Ionicons name="bandage-outline" size={24} paddingRight={10} color="grey" />
-                <Text style={{ fontSize: '13', fontWeight: '900' }}>Counselling</Text>
-            </View>
+                <Text style={{ fontSize: '13', fontWeight: '900' ,textAlign:"center",color:"white"}}>Counselling</Text>
+            </TouchableOpacity>
 
-            <View style={{ alignItems: "center", marginRight: 30, resizeMode: "contain", marginTop: 5 }} >
+            <TouchableOpacity style={{ alignItems: "center", marginRight: 30,marginLeft: 30, resizeMode: "contain", marginTop: 5 }} >
                 <Ionicons name="happy-outline" size={24} paddingRight={10} color="#FFBF00" />
-                <Text style={{ fontSize: '13', fontWeight: '900' }}>Therapy</Text>
-            </View>
+                <Text style={{ fontSize: '13', fontWeight: '900',textAlign:"center" ,color:"white"}}>Therapy</Text>
+            </TouchableOpacity>
 
 
 
